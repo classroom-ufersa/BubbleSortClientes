8196f25d5816722ac35e5e29972b2a7a7381ea48
Thayssz <juuhfreitas4333@gmail.com>
2024-02-08 10:03:18 -0300

    implementando a struct cliente

--- cliente.c
+++ cliente.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+struct cliente{
+    char nome[50];
+    char endereco[50];
+    int codigo;
+};
+
+struct clientes * preenche_cliente(struct clientes *cliente){
+
+    printf("Digite o nome do cliente (ou 0 para encerrar): ");
+    scanf(" %[^\n]", cliente->nome);
+
+    printf("Digite o endereço do cliente: ");
+    scanf(" %[^\n]", cliente->endereco);
+
+    printf("Digite o código do cliente: ");
+    scanf("%d", &cliente->codigo);
+
+    printf("---------------------------\n");
+
+    return cliente;
+}
