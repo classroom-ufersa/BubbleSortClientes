d6299c1d1111d8476cd481a12f64a84094d15fa5
Jean-Richardson <jeanrichardson88@gmail.com>
2024-02-09 13:45:40 -0300

    modificar main.c

--- main.c
+++ main.c
@@ -5,59 +5,47 @@
 #include "cliente.h"
 
 int main(void) {
+    Cliente lista_clientes[100];
+    FILE *arquivo;
+    arquivo = fopen("clientes.txt", "w");
 
-  Cliente* lista_clientes[100];
-  FILE *arquivo;
-  arquivo = fopen("clientes.txt", "w");
-
-  if (arquivo == NULL) {
-    printf("Erro ao criar o arquivo!");
-    return 1;
-  }
-
-  char clientes[100][50];
-  int x = 1;
-  char nome[50];
-  int qtdClientes = 0;
-  // Loop para adicionar clientes ao vetor clientes e ao arquivo
-  while (x == 1) {
-
-    printf("Informe um nome a ser cadastrado: ");
-    scanf(" %[^\n]", nome);
-
-    if (qtdClientes < 100) { // Testa se há espaço no vetor clientes
-      Cliente* novo_cliente = (Cliente *)malloc(sizeof(Cliente));
-      preenche_cliente(novo_cliente);
-      lista_clientes[qtdClientes] = novo_cliente;
-      fprintf(arquivo, "%s\t %s \t %d", novo_cliente->nome, novo_cliente->endereco, novo_cliente->codigo); // Adiciona o nome ao arquivo clientes.txt
-      qtdClientes++;
-    } else {
-      printf("Não há espaço suficiente no vetor");
-      break;
+    if (arquivo == NULL) {
+        printf("Erro ao criar o arquivo!");
+        return 1;
     }
 
-    printf("1 - Cadastrar outro cliente / 0 - Para encerrar o programa.\n");
-    scanf("%d", &x);
-    if (x == 0) {
-      x = 0;
-      fclose(arquivo);
-      for (int i = 0; i < qtdClientes; i++) {
-        printf("%s \n", clientes[i]);
-      }
+    int x = 1;
+    int qtdClientes = 0;
+    while (x == 1) {
+        if (qtdClientes < 100) {
+            preenche_cliente(&lista_clientes[qtdClientes]);
+            qtdClientes++;
+        } else {
+            printf("Não há espaço suficiente no vetor");
+            break;
+        }
+
+        printf("1 - Cadastrar outro cliente / 0 - Para encerrar o programa.\n");
+        scanf("%d", &x);
+        if (x == 0) {
+            x = 0;
+            fclose(arquivo);
+        }
     }
-  }
 
-  bubble_sort(clientes, qtdClientes);
+    bubble_sort(lista_clientes, qtdClientes);
 
-  arquivo = fopen("clientes.txt", "w");
-  if (arquivo == NULL) {
-    printf("Erro ao abrir o arquivo!");
-    return 1;
-  }
+    arquivo = fopen("clientes.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo!");
+        return 1;
+    }
+    fprintf(arquivo, "Nome \t Endereço \t Código"); 
+    for (int i = 0; i < qtdClientes; i++) {
+        fprintf(arquivo, "%s \t %s \t %d \n", lista_clientes[i].nome, lista_clientes[i].endereco, lista_clientes[i].codigo);
+    }
 
-  for (int i = 0; i < qtdClientes; i++) {
-    fprintf(arquivo, "%s \n", clientes[i]);
-  }
+    fclose(arquivo);
 
-  fclose(arquivo);
+    return 0;
 }
