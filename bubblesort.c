09a16c54d28d102eda06448bebbe4a2fcdf896bd
adrianviniciuscs <adrianvinicius.cs@gmail.com>
2024-02-09 08:37:55 -0300

    otimizar bubblesort e adicionar suporte a ordenação de um vetor de Clientes

--- bubblesort.c
+++ bubblesort.c
@@ -1,16 +1,25 @@
+#include <stdbool.h>
 #include <string.h>
+#include "cliente.h"
 
-
-void bubble_sort(char clientes[100][50], int num_clientes) {
-    int i, j;
-    char temp[50];
-    for (i = 0; i < num_clientes - 1; i++) {
-        for (j = 0; j < num_clientes - i - 1; j++) {
-            if (strcmp(clientes[j], clientes[j + 1]) > 0) {
-                strcpy(temp, clientes[j]);
-                strcpy(clientes[j], clientes[j + 1]);
-                strcpy(clientes[j + 1], temp);
-            }
-        }
+void bubble_sort(Cliente clientes[100], int num_clientes) {
+  int i, j;
+  Cliente temp;
+  for (i = 0; i < num_clientes - 1; i++) {
+    bool trocado = false; // sinaliza se aconteceu uma troca entre dois
+                          // elementos no loop interno
+    for (j = 0; j < num_clientes - i - 1; j++) {
+      if (strcmp(clientes[j].nome, clientes[j + 1].nome) > 0) {
+        temp = clientes[j];
+        clientes[j] = clientes[j + 1];
+        clientes[j + 1] = temp;
+        trocado = true;
+      }
     }
+    // se nenhuma troca foi realizada pelo loop interno,
+    // o código realiza um break
+    if (trocado == false) {
+      break;
+    }
+  }
 }
